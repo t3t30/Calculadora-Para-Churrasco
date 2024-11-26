be74f67e0f287aabe5ad54c117f2399a029e0e31
t3t30 <mateusbie6@gmail.com>
2024-11-25 22:04:53 -0300

    continua mostrando 0 nos resultados

--- app/src/main/java/com/example/calculadorachurrasco/ResultActivity.kt
+++ app/src/main/java/com/example/calculadorachurrasco/ResultActivity.kt
@@ -1,10 +1,16 @@
 package com.example.calculadorachurrasco
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+const val  Key_carne= "ResultActivity.KEY_CARNE"
+const val Key_cerveja= "ResultActivity.KEY_CERVEJA"
+const val Key_refrigerante= "ResultActivity.KEY_REFRIGERANTE"
+
+
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,12 +20,21 @@ class ResultActivity : AppCompatActivity() {
 
 
         // recuperação de resultados
+        val resultcarne = intent.getFloatExtra(Key_carne, 0f)
+        val resultcerveja = intent.getFloatExtra( Key_cerveja, 0f)
+        val resultrefrigerante = intent.getFloatExtra( Key_refrigerante, 0f)
 
-        val resultado= intent.getFloatExtra("123", 0f)
+        val carne = findViewById<TextView>(R.id.tv_carne)
+        val carneFormat = String.format("%.2f", resultcarne)
+        "${carneFormat}(kg)".also { carne.text = it }
 
+        val cerveja = findViewById<TextView>(R.id.tv_cerveja)
+        val cervejaFormat = String.format("%.2f", resultcerveja)
+        "${cervejaFormat}(Litros)".also { cerveja.text = it }
 
-
-
+        val refrigerante = findViewById<TextView>(R.id.tv_refrigerante)
+        val refrigeranteFormat = String.format("%.2f", resultrefrigerante)
+        "${refrigeranteFormat}(Litros)".also { refrigerante.text = it }
 
 
     }
